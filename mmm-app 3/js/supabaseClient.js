d5d798fd74a2ad96981feb28b2f861fe2c087a41
aurabotsubjects <aurabotsubjects@zohomail.com.au>
2026-07-15 17:31:26 +1200

    Delete mmm-app 3 directory

--- mmm-app 3/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// Requires the Supabase JS UMD build to be loaded first (see <script> tag in HTML)
-// and window.MMM_CONFIG to be set (config.js).
-window.sb = supabase.createClient(
-  window.MMM_CONFIG.SUPABASE_URL,
-  window.MMM_CONFIG.SUPABASE_ANON_KEY
-);
